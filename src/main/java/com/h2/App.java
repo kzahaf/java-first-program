f2faad85901b935456f964ec3e0598f6cc615916
kzahaf <khalil.zahaf@capgemini.com>
2020-10-22 19:48:44 +0200

    99

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -4,14 +4,14 @@ package com.h2;
  * Hello world 2!
  *
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
+    public static void main(  String[] args )
     {
         System.out.println( "Hello World! 2" );
     }
 
     public static int doubleTheNumber(int number) {
-        return -100;
+        return -10099;
     }
 }
