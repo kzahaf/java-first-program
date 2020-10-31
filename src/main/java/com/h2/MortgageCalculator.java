0826bcd22f224655508ab77465899b4e2f9a5304
kzahaf <khalil.zahaf@capgemini.com>
2020-10-31 22:46:14 +0100

    Module 3 successful

---
+++ src/main/java/com/h2/MortgageCalculator.java
@@ -0,0 +1,45 @@
+package com.h2;
+
+import java.text.DecimalFormat;
+
+public class    MortgageCalculator {
+    private long loanAmount;
+    private int termInYears;
+    private float annualRate;
+    private double monthlyPayment;
+    public MortgageCalculator(long loanAmount, int termInYears, float annualRate){
+     this.loanAmount=loanAmount;
+     this.annualRate=annualRate;
+     this.termInYears=termInYears;
+    }
+    private int getNumberOfPayments() {
+     return termInYears*12;
+    }
+    private float getMonthlyInterestRate(){
+     float interestRate;
+     interestRate=annualRate/100;
+     return interestRate/12;
+    }
+    public void calculateMonthlyPayment(){
+      long p=loanAmount;
+      float r =getMonthlyInterestRate();
+      int n=getNumberOfPayments();
+      double M;
+      M= p * (((r * Math.pow(1 + r, n))) / ((Math.pow((1 + r), n)) - 1));
+      this.monthlyPayment=M;
+    }
+    public String toString() {
+        DecimalFormat df=new DecimalFormat("####0.00");
+        return "monthlyPayment: " + df.format(monthlyPayment);
+
+    }
+    public static void main(String[] args){
+        long loanAmount =Long.parseLong(args[0]);
+        int termInYears = Integer.parseInt(args[1]);
+        float annualRate=Float.parseFloat(args[2]);
+        MortgageCalculator calculator=new MortgageCalculator(loanAmount,termInYears,annualRate );
+        calculator.calculateMonthlyPayment();
+        System.out.println(calculator.toString());
+
+    }
+}
