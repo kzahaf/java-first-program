6ac3cec6341d16db8ff529797e1b630f038c4772
kzahaf <khalil.zahaf@capgemini.com>
2020-11-02 17:44:21 +0100

    Module 5 successful_V2

--- src/main/java/com/h2/BestLoanRates.java
+++ src/main/java/com/h2/BestLoanRates.java
@@ -6,7 +6,7 @@ public class BestLoanRates {
     public static final Map<Integer, Float> bestRates = Map.of(1, 5.50f, 2, 3.45f, 3, 2.67f);
     public static void main( String[] args) {
         Scanner scanner=new Scanner(System.in);
-        System.out.print("Enter your name\n");
+        System.out.println("Enter your name");
         String name=scanner.nextLine();
         //String name="Khalil";
         System.out.print("Hello "+ name+"\n");
